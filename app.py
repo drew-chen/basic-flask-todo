8ff1a9b05d432697e34ba10061917724fa7b7f3a
Andrew Chen <chen_andrew@berkeley.edu>
2020-07-04 01:09:48 -0700

    Setup SQLite database

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.String(200), nullable=False)
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
+	
+	def __repr__(self):
+		return '<Task %r>' % self.id
+		# return '<Task {id!r}>'.format(id=self.id)
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	return render_template('index.html')
+	if request.method == 'POST':
+		return 'Task added'
+	else:
+		return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
