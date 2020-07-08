05bea7760b8f6f9d62fba05713c6e8a47aff9e1f
Andrew Chen <chen_andrew@berkeley.edu>
2020-07-07 23:37:06 -0700

    Add deletion

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	content = db.Column(db.String(200), nullable=False)
 	date_created = db.Column(db.DateTime, default=datetime.utcnow)
-	
+	# 31:33
 	def __repr__(self):
 		return '<Task %r>' % self.id
 		# return '<Task {id!r}>'.format(id=self.id)
@@ -30,6 +30,18 @@ def index():
 	else:
 		tasks = Todo.query.order_by(Todo.date_created).all()
 		return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+	task_to_delete = Todo.query.get_or_404(id)
+	try:
+		db.session.delete(task_to_delete)
+		db.session.commit()
+		return redirect('/')
+	except:
+		return 'There was a problem deleting that task'
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
